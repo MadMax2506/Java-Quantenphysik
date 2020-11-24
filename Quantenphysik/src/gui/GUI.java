2d7ae8be6cf1777615a0ed398477cc705d6da201
JanorschkeMax25 <JanorschkeMax25@BKA-TS1.bka.intern>
2020-11-24 09:45:14 +0100

    Vorbereitung

--- Quantenphysik/src/gui/GUI.java
+++ Quantenphysik/src/gui/GUI.java
@@ -1,17 +1,20 @@
 package gui;
 
-import java.awt.BorderLayout;
-import java.awt.EventQueue;
+import java.awt.CardLayout;
+import java.awt.Color;
 
 import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 
 public class GUI extends JFrame {
 
-	/**
-	 * 
-	 */
+	public final String HOME_PANEL_NAME = "HOME";
+	public final String ELEKTONEN_PANEL_NAME = "ELEKTRONEN";
+	
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
 
@@ -19,12 +22,42 @@ public class GUI extends JFrame {
 	 * Create the frame.
 	 */
 	public GUI() {
+		setTitle("Quantenphysik");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 450, 450);
+		
+		JMenuBar menuBar = new JMenuBar();
+		setJMenuBar(menuBar);
+		
+		JMenu mnRechenoperation = new JMenu("Rechenoperationen");
+		menuBar.add(mnRechenoperation);
+		
+		JMenuItem mntmHome = new JMenuItem("Startseite");
+		mnRechenoperation.add(mntmHome);
+		mntmHome.addActionListener((e) -> show_panel(HOME_PANEL_NAME));
+		
+		JMenuItem mntmElektronen = new JMenuItem("Wellencharakter von Elektronen");
+		mnRechenoperation.add(mntmElektronen);
+		mntmElektronen.addActionListener((e) -> show_panel(ELEKTONEN_PANEL_NAME));
+		
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		contentPane.setLayout(new CardLayout(0, 0));
+		contentPane.setBackground( Color.BLACK );
+		
+		JPanel homePanel = new HomePanel();
+		contentPane.add(homePanel, HOME_PANEL_NAME);
+		
+		JPanel elektronenPanel = new ElektonenPanel();
+		contentPane.add(elektronenPanel, ELEKTONEN_PANEL_NAME);
+		
+		show_panel(HOME_PANEL_NAME);
+	}
+	
+	private void show_panel(String name) {
+		CardLayout c = (CardLayout)contentPane.getLayout();
+		c.show(contentPane, name);
 	}
 
 }
