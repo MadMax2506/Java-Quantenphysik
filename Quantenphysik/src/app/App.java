403874b4eebd5eebf068743c5cbba72f3b4dd2d4
Mad Max <mm.madmax2506@gmail.com>
2021-01-29 21:56:13 +0100

    create user folder

--- Quantenphysik/src/app/App.java
+++ Quantenphysik/src/app/App.java
@@ -3,17 +3,18 @@ import java.awt.EventQueue;
 import java.io.File;
 
 import gui.main.GUI;
+import utils.Directory;
 
 public class App {
 	
-	public static final File user_folder = new File(System.getProperty("user.home") + "/quantenphysik");
+	public static final File user_folder = new File(System.getProperty("user.home") + "/.quantenphysik");
 	
 	public static void main(String[] args) {
 		new App();
 	}
 	
 	public App() {
-		user_folder.mkdirs();
+		Directory.make(user_folder);
 		
 		EventQueue.invokeLater(() -> start_app_gui());
 	}
